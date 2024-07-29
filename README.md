ea6312eed9f41f2b81e02edd9323cda79e2b60b0
JPBR <jpberrocal@gmail.com>
2024-07-29 11:03:58 -0600

    Finished Site

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ To excute run
 npm run dev
 ```
 
-https://youtu.be/tS7upsfuxmo?t=7865
+https://youtu.be/tS7upsfuxmo?t=9482
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,14 @@
 
-function Button({ label, iconURL, backgroundColor, borderColor, textColor }) {
+function Button({ label, iconURL, backgroundColor, borderColor, textColor, fullWidth }) {
     return (
-        <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
-        ${backgroundColor
+        <button className={`flex justify-center items-center gap-2 px-7 py-4 border     font-montserrat text-lg leading-none rounded-full
+            ${backgroundColor
                 ? `${backgroundColor} ${borderColor} ${textColor}`
                 : 'bg-coral-red text-white border-coral-red'
 
-            }`}>
+            }
+            ${fullWidth && 'w-full'}
+            `}>
             {label}
             {
                 iconURL && (
